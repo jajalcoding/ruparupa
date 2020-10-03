d76258f72a4ed4fd1f0298c925db640e950e932f
jajalcoding <70680913+jajalcoding@users.noreply.github.com>
2020-10-03 17:46:05 +0700

    v2
    updated with error handling and loop and wait until success

--- cmd2cli.py
+++ cmd2cli.py
@@ -1,6 +1,8 @@
-# cmd2cli v 1.0
+# cmd2cli v 2.0
 # by TAS AthionXX
 # very useful for ssh command that needs password
+# with additional handler and loop if not succesful will wait 5 seconds
+
 # Usage :
 # python cmd2cli.py 192.168.1.99 admin pass1234 fgtcli.txt
 # 
@@ -12,9 +14,11 @@
 # end
 
 from paramiko import SSHClient, AutoAddPolicy
+from paramiko.auth_handler import AuthenticationException, SSHException
 import string
 import time
 import sys
+import socket
 
 def ssh_host(ipadd,user,pwd,command):
     client = SSHClient()
@@ -29,11 +33,26 @@ def ssh_host(ipadd,user,pwd,command):
 def ssh_cli_command(ipadd,user,pwd,namafile):
     client = SSHClient()
     client.set_missing_host_key_policy(AutoAddPolicy())
-    client.connect(ipadd,username=user,password=pwd)
-    remote_shell = client.invoke_shell()
+    try:
+        client.connect(ipadd,username=user,password=pwd)
+        remote_shell = client.invoke_shell()
 
-    f = open(namafile, "r")
-    
+    except AuthenticationException:
+        print("Authentication failed, please verify your credentials !")
+        return False
+    except SSHException as sshException:
+        print("Unable to establish SSH connection: %s" % sshException)
+        return False
+    except socket.error:
+        print("Unable to connect to host "+ipadd)
+        return False
+
+    try:
+        f = open(namafile, "r")
+    except :
+        print("File "+namafile+" is not found !")
+        return False
+
     for line in f:
         if ( len(line)>1 ):
             remote_shell.send(line.strip()+"\n")
@@ -45,7 +64,7 @@ def ssh_cli_command(ipadd,user,pwd,namafile):
     f.close()
 
     client.close()
-
+    return True
 
 def main():
     if len(sys.argv) < 4:
@@ -57,8 +76,20 @@ def main():
     username = sys.argv[2]
     passw = sys.argv[3]
     namafile = sys.argv[4]
+    waittime = 5
+    result = False
+    s = time.perf_counter()
+    
+    while ( result==False ):
+        result = ssh_cli_command(hostname,username,passw,namafile)
+        if ( result==False ):
+            print ("Can not connect to "+hostname+", will retry in "+str(waittime)+" seconds.....")
+            print ("Or press Ctrl+C to break !!")
+            time.sleep(waittime)
+    
+    elapsed = time.perf_counter() - s
+    print(f"{__file__} executed in {elapsed:0.2f} seconds.")
 
-    ssh_cli_command(hostname,username,passw,namafile)
 
 if __name__ == '__main__':
    main()
